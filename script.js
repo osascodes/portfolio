7272f39482c9c684e2ddd11da242d332a8398637
Benjamin Ayere <ayerebenjaminosahon@gmail.com>
2025-09-07 11:28:21 +0100

    Updated portfolio

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
+// DOM Elments
 const hamburger = document.getElementById('hamburger');
 const drawer = document.getElementById('drawer');
 const drawerLinks = document.querySelectorAll('.drawer-links a');
 const zoomElem = document.getElementById("zoom-specialist");
 const scrollBtn = document.getElementById('scrollToTop');
-let progress =0;
 
+// Variable
+let progress =0;
+let zoomIndex = 0;
+
+// Scroll Progress
 window.addEventListener('scroll', () => {
   const homeSectionHeight = document.getElementById('home').offsetHeight;
   const scrollPos = window.scrollY;
   const docHeight = document.documentElement.scrollHeight - window.innerHeight;
+  
   progress = (scrollPos / docHeight) * 100;
 
   // Update background fill
@@ -21,7 +27,7 @@ window.addEventListener('scroll', () => {
     scrollBtn.style.display = 'none';
   }
 });
-
+  // Scroll To Top Button
 scrollBtn.addEventListener('click', (e) => {
   e.preventDefault();
   window.scrollTo({
@@ -30,13 +36,13 @@ scrollBtn.addEventListener('click', (e) => {
   });
 });
 
-
+  // Zooming Text
 const zoomWords = ["Creative", "Developer", "Designer"];
-let zoomIndex = 0;
+
 
 function zoomChange() {
-  zoomElem.style.transform = "scale(0)";
-  zoomElem.style.opacity = 0;
+  zoomElem.classList.remove("visible");
+  zoomElem.classList.add("hidden");
   setTimeout(() => {
     zoomElem.textContent = zoomWords[zoomIndex];
     zoomElem.style.transform = "scale(1)";
@@ -47,7 +53,7 @@ function zoomChange() {
 setInterval(zoomChange, 2000);
 zoomChange();
 
-
+  // Hamburger Menu
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   drawer.classList.toggle('active');
